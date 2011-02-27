62b05a0b4db81917b757ff7efac441c4cabdd90d
Curtis Bridges <curtis.bridges@gmail.com>
2011-02-27 11:07:31 -0500

    Initial commit.

---
+++ src/com/curtisbridges/swing/Scroller.java
@@ -0,0 +1,82 @@
+package com.curtisbridges.swing;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.text.DefaultCaret;
+
+public class Scroller extends JFrame {
+    Timer       timer;
+    TimerTask   task;
+
+    JTabbedPane tabs;
+    JTextArea   textArea;
+    JScrollPane scrollPane;
+
+    public Scroller() {
+        super("Scroller Test");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createComponents();
+        createTimerAndTask();
+    }
+
+    protected void createComponents() {
+        Container container = this.getContentPane();
+        container.setLayout(new BorderLayout());
+
+        tabs = new JTabbedPane();
+
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+
+        DefaultCaret caret = (DefaultCaret) textArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        caret.setVisible(false);
+
+        scrollPane = new JScrollPane(textArea);
+
+        tabs.add("Status", scrollPane);
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(200, 50));
+
+        container.add(panel, BorderLayout.NORTH);
+        container.add(tabs, BorderLayout.CENTER);
+
+        JLabel label = new JLabel("Theoretical status bar.");
+        container.add(label, BorderLayout.SOUTH);
+    }
+
+    protected void createTimerAndTask() {
+        timer = new Timer("ScrollerThread");
+        task = new TimerTask() {
+            public void run() {
+                Scroller.this.doText();
+            }
+        };
+
+        timer.schedule(task, 250, 250);
+    }
+
+    public void doText() {
+        textArea.append("Hello, World!\n");
+    }
+
+    public static void main(String[] args) {
+        Scroller scroller = new Scroller();
+
+        scroller.setSize(640, 480);
+        scroller.setVisible(true);
+    }
+}
